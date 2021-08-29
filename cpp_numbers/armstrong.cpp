f8635e51711977f875a06d9fadff4597cf47c36a
FathimaNejiya <85240114+FathimaNejiya@users.noreply.github.com>
2021-08-29 20:08:33 +0530

    add armstrong.cpp

--- cpp_numbers/armstrong.cpp
+++ cpp_numbers/armstrong.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n,t,r,s=0;
+    cout << "enter a number:";
+    cin >> n;
+    t = n;
+
+    while(t != 0)
+    {
+        r = t % 10;
+        s += r * r * r;
+        t /= 10;
+    }
+
+    if(n == s)
+      cout << "armstrong";
+    else
+      cout << "not armstrong";
+    return 0;
+}
